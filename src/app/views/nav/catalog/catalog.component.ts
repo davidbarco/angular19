13ef5634f4bfba9ee882b550fe65f776a7076bb9
davidbarco <66650959+davidbarco@users.noreply.github.com>
2024-12-12 16:05:26 -0300

    se crea componente de titulo

--- src/app/views/nav/catalog/catalog.component.ts
+++ src/app/views/nav/catalog/catalog.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CardCatalogComponent } from "../../../shared/components/card-catalog/card-catalog.component";
+import { TitleComponent } from "../../../shared/components/title/title.component";
 
 @Component({
   selector: 'app-catalog',
-  imports: [CardCatalogComponent],
+  imports: [CardCatalogComponent, TitleComponent],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.css'
 })
